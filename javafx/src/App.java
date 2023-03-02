0db22260a06f21e86eb4c9485645709e014a3a0c
Wilmer Andres Duque Olaya <44647699+wilmerd1@users.noreply.github.com>
2023-03-02 17:09:04 -0500

    interfaz Conversor

--- javafx/src/App.java
+++ javafx/src/App.java
@@ -5,37 +5,28 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
- 
+
 public class App extends Application {
     @Override
     public void start(Stage primaryStage) {
-      
-  
-  
-  Parent root;
-try {
-    root = FXMLLoader.load(getClass().getResource("MainScene.fxml"));
 
-    Scene scene = new Scene(root);
-  
-  
-  
-  
-    primaryStage.setTitle("Hello World!");
-          primaryStage.setScene(scene);
-          primaryStage.show();
+        Parent root;
+        try {
+            root = FXMLLoader.load(getClass().getResource("MainScene.fxml"));
 
-} catch (IOException e) {
-    
-    e.printStackTrace();
-}
-  
+            Scene scene = new Scene(root);
+            primaryStage.setTitle("Hello World!");
+            primaryStage.setScene(scene);
+            primaryStage.show();
+
+        } catch (IOException e) {
+
+            e.printStackTrace();
+        }
+
     }
 
-
-    
- 
- public static void main(String[] args) {
+    public static void main(String[] args) {
         launch(args);
     }
 }
--- javafx/src/MainSceneController.java
+++ javafx/src/MainSceneController.java
@@ -7,21 +7,6 @@ import javafx.stage.Stage;
 
 public class MainSceneController {
 
-    @FXML
-    private ComboBox<?> cbDviisa2;
     
-    @FXML
-    private ComboBox<?> cbDivisa1;
-
-    @FXML
-    private TextField tfTltle;
-
-    @FXML
-    void btnOKClicked(ActionEvent event) {
-        Stage mainWindows = (Stage) tfTltle.getScene().getWindow();
-        String title = tfTltle.getText();
-        mainWindows.setTitle(title);
-
-    }
 
 }
