48f43a033b1af4dc6f89d2f7f445242d16217238
wilmerd1 <44647699+wilmerd1@users.noreply.github.com>
2023-03-02 21:40:42 -0500

    Mejora Interfaz

--- javafx/src/App.java
+++ javafx/src/App.java
@@ -1,28 +1,74 @@
-import java.io.IOException;
+
 
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.input.MouseButton;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.paint.Color;
+
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 public class App extends Application {
+    private double initialX;
+    private double initialY;
+
     @Override
-    public void start(Stage primaryStage) {
+    public void start(Stage primaryStage) throws Exception {
 
-        Parent root;
-        try {
-            root = FXMLLoader.load(getClass().getResource("MainScene.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("MainScene.fxml"));
 
-            Scene scene = new Scene(root);
-            primaryStage.setTitle("Hello World!");
-            primaryStage.setScene(scene);
-            primaryStage.show();
+        root.setOnMousePressed(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent me) {
+                initialX= me.getSceneX();
+                initialY = me.getSceneY();
+             
+            }
+          });
+        
+          root.setOnMouseDragged(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent me) {
+              
+               
+                root.getScene().getWindow().setX(me.getScreenX() - initialX);
+                
+                root.getScene().getWindow().setY(me.getScreenY() - initialY);
+              
+            }
+          });
+        
 
-        } catch (IOException e) {
+       /*  root.setOnMousePressed(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                xOffset = event.getSceneX();
+                yOffset = event.getSceneY();
+            }
 
-            e.printStackTrace();
-        }
+        });
+
+        root.setOnMouseDragged(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                primaryStage.setX(event.getSceneX() - xOffset);
+                primaryStage.setY(event.getSceneY() - yOffset);
+            }
+
+        });*/
+
+
+        primaryStage.initStyle(StageStyle.TRANSPARENT);
+        Scene scene = new Scene(root);
+        scene.setFill(Color.TRANSPARENT);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Conversor Alura");
+      //  primaryStage.initModality(Modality.WINDOW_MODAL);
+        primaryStage.show();
 
     }
 
--- javafx/src/MainSceneController.java
+++ javafx/src/MainSceneController.java
@@ -1,12 +1,92 @@
-
-import javafx.event.ActionEvent;
+import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-import javafx.stage.Stage;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
 
 public class MainSceneController {
 
-    
+    @FXML
+    private ImageView arrowDivisas;
+
+    @FXML
+    private ImageView arrowExit;
+
+    @FXML
+    private ImageView arrowTemp;
+
+    @FXML
+    private ImageView btnExit;
+
+    @FXML
+    private ComboBox<?> cbxDivisa1;
+
+    @FXML
+    private ComboBox<?> cbxDivisa11;
+
+    @FXML
+    private ComboBox<?> cbxDivisa2;
+
+    @FXML
+    private ComboBox<?> cbxDivisa21;
+
+    @FXML
+    private AnchorPane divisasPanel;
+
+    @FXML
+    private Label lblDivisa1;
+
+    @FXML
+    private Label lblDivisa11;
+
+    @FXML
+    private Label lblDivisa2;
+
+    @FXML
+    private Label lblDivisa21;
+
+    @FXML
+    private AnchorPane tempPanel;
+
+    @FXML
+    private TextField txtDivisa;
+
+    @FXML
+    private TextField txtDivisa1;
+
+    @FXML
+    private TextField txtResultado;
+
+    @FXML
+    private TextField txtResultado1;
+
+    @FXML
+    void getOnDivisasButtonClicked(MouseEvent event) {
+        divisasPanel.setVisible(true);
+        arrowDivisas.setVisible(true);
+        tempPanel.setVisible(false);
+        arrowTemp.setVisible(false);
+        arrowExit.setVisible(false);
+    }
+
+    @FXML
+    void getOnExitButtonClicked(MouseEvent event) {
+        Platform.exit();
+        System.exit(0);
+    }
+
+    @FXML
+    void getOnTemperaturaButtonClicked(MouseEvent event) {
+
+        tempPanel.setVisible(true);
+        arrowTemp.setVisible(true);
+        divisasPanel.setVisible(false);
+        arrowDivisas.setVisible(false);
+        arrowExit.setVisible(false);
+
+    }
 
 }
